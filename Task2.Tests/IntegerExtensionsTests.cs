255339e4f3fbb8e5821c1938c8b4d8a43bbca2a8
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-01-25 16:17:26 +0300

    feat: add implementation and unit tests for GetGcdByEuclidean method.

--- Task2.Tests/IntegerExtensionsTests.cs
+++ Task2.Tests/IntegerExtensionsTests.cs
@@ -14,36 +14,63 @@ namespace Task2.Tests
         [TestCase(2672, 5678, ExpectedResult = 334)]
         [TestCase(10927782, 6902514, ExpectedResult = 846)]
         [TestCase(1590771464, 1590771620, ExpectedResult = 4)]
-        [Order(1)]
-        public int GcdTests_WithAllPositiveNumbers(int a, int b) => GetGcd(a, b);
-
-        
-        [Timeout(3000)]
-        [Order(3)]
+        [TestCase(3, 15, ExpectedResult = 3)]
+        [TestCase(18, 0, ExpectedResult = 18)]
+        [TestCase(-7, -7, ExpectedResult = 7)]
         [TestCase(10927782, -6902514, ExpectedResult = 846)]
         [TestCase(-1590771464, 1590771620, ExpectedResult = 4)]
         [TestCase(-10234562, -7872334, ExpectedResult = 2)]
         [TestCase(1590771464, -1590771620, ExpectedResult = 4)]
         [TestCase(-10234567, -234568989, ExpectedResult = 97)]
-        public int GcdTests_WithSomeNegativeNumbers(int a, int b) => GetGcd(a, b);
-
         [TestCase(945, 0, ExpectedResult = 945)]
         [TestCase(0, -301, ExpectedResult = 301)]
         [TestCase(10927782, 0, ExpectedResult = 10927782)]
         [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
+        [Order(1)]
+        public int GcdByEuclideanTests_WithTwoNumbers(int a, int b) => GetGcdByEuclidean(a, b);
+
+        
+        [Timeout(3000)]
+        [Order(3)]
+        [TestCase(3, -3, 3, ExpectedResult = 3)]
+        [TestCase(15, 5, 45, ExpectedResult = 5)]
+        [TestCase(0, 0, -1, ExpectedResult = 1)]
+        [TestCase(5, 5, 5, ExpectedResult = 5)]
+        [TestCase(100, 60, 16, ExpectedResult = 4)]
+        [TestCase(100, -100, -50, ExpectedResult = 50)]
+        [TestCase(1, 2, 3, ExpectedResult = 1)]
+        [TestCase(-1, -2, -3, ExpectedResult = 1)]
+        [TestCase(100, 60, 40, ExpectedResult = 20)]
+        public int GcdByEuclideanTests_WithThreeNumbers(int a, int b, int c) => GetGcdByEuclidean(a, b, c);
+
+        [TestCase(123413, 943578, 123413, 943578, 943578, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(12, 21, 91, 17, 0, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(-10, 35, 90, 55, -105, ExpectedResult = 5)]
+        [TestCase(1, 213124, -54654, -123124, 65765, 44444, -7, 1234567, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(0, 1, 0, 0, ExpectedResult = 1)]
+        [TestCase(18, 3, 9, 6, ExpectedResult = 3)]
+        [TestCase(0, 0, 1, 0, ExpectedResult = 1)]
         [Timeout(3000)]
         [Order(4)]
-        public int GcdTests_WithOneZeroNumber(int a, int b) => GetGcd(a, b);
+        public int GcdByEuclideanTests_ManyNumber(params int[] numbers) => GetGcdByEuclidean(numbers);
 
         [TestCase(int.MaxValue, int.MinValue + 1, ExpectedResult = int.MaxValue)]
         [Property("Mark", 2)]
         [Timeout(10000)]
         [Order(5)]
-        public int GcdTest_WithMaxAndMinIntegerNumbers(int a, int b) => GetGcd(a, b);
+        public int GcdByEuclideanTest_WithMaxAndMinIntegerNumbers(int a, int b) => GetGcdByEuclidean(a, b);
 
         [Test, Order(2)]
-        public void GcdTest_WithTwoZeroNumbers_ThrowArgumentException() =>
-            Assert.Throws<ArgumentException>(() => GetGcd(0, 0),
-                "Two numbers cannot be 0 at the same time.");
+        public void GcdByEuclideanTest_WithAllZeroNumbers_ThrowArgumentException() =>
+            Assert.Throws<ArgumentException>(() => GetGcdByEuclidean(0, 0),
+                "All numbers cannot be 0 at the same time.");
+
+        [Test]
+        public void GcdByEuclideanTest_WithOneParameter_ThrowArgumentException() =>
+            Assert.Throws<ArgumentException>(() => GetGcdByEuclidean(24));
+
+        [Test]
+        public void GcdByEuclideanTest_WithIntMinValue_ThrowArgumentOutOfRangeException() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => GetGcdByEuclidean(25, int.MinValue));
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task2
 {
@@ -14,7 +15,7 @@ namespace Task2
         /// <returns>Returns GCD of two numbers.</returns>
         /// <exception cref="ArgumentException">Thrown when two numbers are zero.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
-        public static int GetGcd(int a, int b)
+        public static int GetGcdByEuclidean(int a, int b)
         {
             if (a == 0 && b == 0)
             {
@@ -26,23 +27,108 @@ namespace Task2
                 throw new ArgumentOutOfRangeException($"{int.MinValue} is out of range.");
             }
 
-            if (a == 0)
-            {
-                return Math.Abs(b);
-            }
-
-            if (b == 0)
-            {
-                return Math.Abs(a);
-            }
-
             int absA = Math.Abs(a);
             int absB = Math.Abs(b);
             return absA >= absB ? EuclideanAlgorithm(absA, absB) : EuclideanAlgorithm(absB, absA);
         }
 
+        /// <summary>
+        ///   <para>Gets the GCD of three integer numbers.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second number.</param>
+        /// <param name="c">Third number.</param>
+        /// <returns>Returns GCD of three numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when three numbers are zero.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByEuclidean(int a, int b, int c)
+        {
+            if (a == 0 && b == 0 && c == 0)
+            {
+                throw new ArgumentException("Three numbers cannot be 0 at the same time.");
+            }
+
+            if (a == int.MinValue || b == int.MinValue || c == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException($"{int.MinValue} is out of range.");
+            }
+
+            int absA = Math.Abs(a);
+            int absB = Math.Abs(b);
+            int absC = Math.Abs(c);
+
+            int temporaryResult = absA > absB ? EuclideanAlgorithm(absA, absB) : EuclideanAlgorithm(absB, absA);
+            return absC > temporaryResult ? EuclideanAlgorithm(absC, temporaryResult) : EuclideanAlgorithm(temporaryResult, absC);
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of several integer numbers.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="numbers">Given numbers.</param>
+        /// <returns>Returns GCD of several numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are zero or less than two numbers was given.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByEuclidean(params int[] numbers)
+        {
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException("There are should be at least two parameters.");
+            }
+
+            var gcdsArray = new List<int>(numbers);
+            bool hasAllZeroes = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == int.MinValue)
+                {
+                    throw new ArgumentOutOfRangeException($"{int.MinValue} is out of range.");
+                }
+
+                gcdsArray[i] = Math.Abs(numbers[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    hasAllZeroes = false;
+                    break;
+                }
+            }
+
+            if (hasAllZeroes)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.");
+            }
+
+            gcdsArray.Sort();
+            for (int i = 0; i < gcdsArray.Count - 1; i++)
+            {
+                if (gcdsArray[i] == 0)
+                {
+                    continue;
+                }
+
+                gcdsArray[i + 1] = EuclideanAlgorithm(gcdsArray[i + 1], gcdsArray[i]);
+            }
+
+            return gcdsArray[gcdsArray.Count - 1];
+        }
+
         private static int EuclideanAlgorithm(int absA, int absB)
         {
+            if (absA == 0)
+            {
+                return absB;
+            }
+
+            if (absB == 0)
+            {
+                return absA;
+            }
+
             int remainder = absA - absB;
             while (remainder != 0)
             {

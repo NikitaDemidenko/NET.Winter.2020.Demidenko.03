7868bd6de8633b6ae25c5daef7ef9e2765e6c69b
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-01-25 19:43:38 +0300

    feat: add implementation and unit tests for GetGdcByStein method.

--- Task2.Tests/IntegerExtensionsTests.cs
+++ Task2.Tests/IntegerExtensionsTests.cs
@@ -6,6 +6,8 @@ namespace Task2.Tests
 {
     public class IntegerExtensionsTests
     {
+        #region GDC Euclidian algorithm tests
+
         [TestCase(30, 12, ExpectedResult = 6)]
         [TestCase(1, 1, ExpectedResult = 1)]
         [TestCase(8, 9, ExpectedResult = 1)]
@@ -26,12 +28,10 @@ namespace Task2.Tests
         [TestCase(0, -301, ExpectedResult = 301)]
         [TestCase(10927782, 0, ExpectedResult = 10927782)]
         [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
-        [Order(1)]
         public int GcdByEuclideanTests_WithTwoNumbers(int a, int b) => GetGcdByEuclidean(a, b);
 
         
         [Timeout(3000)]
-        [Order(3)]
         [TestCase(3, -3, 3, ExpectedResult = 3)]
         [TestCase(15, 5, 45, ExpectedResult = 5)]
         [TestCase(0, 0, -1, ExpectedResult = 1)]
@@ -51,18 +51,21 @@ namespace Task2.Tests
         [TestCase(18, 3, 9, 6, ExpectedResult = 3)]
         [TestCase(0, 0, 1, 0, ExpectedResult = 1)]
         [Timeout(3000)]
-        [Order(4)]
         public int GcdByEuclideanTests_ManyNumbers(params int[] numbers) => GetGcdByEuclidean(numbers);
 
         [TestCase(int.MaxValue, int.MinValue + 1, ExpectedResult = int.MaxValue)]
         [Property("Mark", 2)]
         [Timeout(10000)]
-        [Order(5)]
         public int GcdByEuclideanTest_WithMaxAndMinIntegerNumbers(int a, int b) => GetGcdByEuclidean(a, b);
 
-        [Test, Order(2)]
-        public void GcdByEuclideanTest_WithAllZeroNumbers_ThrowArgumentException() =>
+        [Test]
+        public void GcdByEuclideanTest_WithTwoZeroNumbers_ThrowArgumentException() =>
             Assert.Throws<ArgumentException>(() => GetGcdByEuclidean(0, 0),
+                "Two numbers cannot be 0 at the same time.");
+
+        [Test]
+        public void GcdByEuclideanTest_WithAllZeroNumbers_ThrowArgumentException() =>
+            Assert.Throws<ArgumentException>(() => GetGcdByEuclidean(0, 0, 0, 0, 0, 0, 0),
                 "All numbers cannot be 0 at the same time.");
 
         [Test]
@@ -86,5 +89,66 @@ namespace Task2.Tests
             _ = GetGcdByEuclidean(out long time, numbers);
             Assert.That(time > 1);
         }
+
+        #endregion
+
+        #region GDC Euclidian algorithm tests
+
+        [Test]
+        public void GcdBySteinTest_WithAllZeroNumbers_ThrowArgumentException() =>
+           Assert.Throws<ArgumentException>(() => GetGcdByStein(0, 0),
+               "All numbers cannot be 0 at the same time.");
+
+        [Test]
+        public void GcdBySteinTest_WithOneParameter_ThrowArgumentException() =>
+            Assert.Throws<ArgumentException>(() => GetGcdByStein(24));
+
+        [Test]
+        public void GcdBySteinTest_WithIntMinValue_ThrowArgumentOutOfRangeException() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => GetGcdByStein(25, int.MinValue));
+
+        [TestCase(10927782, -6902514, ExpectedResult = 846)]
+        [TestCase(-1590771464, 1590771620, ExpectedResult = 4)]
+        [TestCase(-10234562, -7872334, ExpectedResult = 2)]
+        [TestCase(1590771464, -1590771620, ExpectedResult = 4)]
+        [TestCase(-10234567, -234568989, ExpectedResult = 97)]
+        [TestCase(945, 0, ExpectedResult = 945)]
+        [TestCase(0, -301, ExpectedResult = 301)]
+        [TestCase(10927782, 0, ExpectedResult = 10927782)]
+        [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
+        public int GcdBySteinTests_WithTwoNumbers(int a, int b) => GetGcdByEuclidean(a, b);
+
+        [TestCase(100, 60, 16, ExpectedResult = 4)]
+        [TestCase(100, -100, -50, ExpectedResult = 50)]
+        [TestCase(1, 2, 3, ExpectedResult = 1)]
+        [TestCase(-1, -2, -3, ExpectedResult = 1)]
+        [TestCase(100, 60, 40, ExpectedResult = 20)]
+        public int GcdBySteinTests_WithThreeNumbers(int a, int b, int c) => GetGcdByEuclidean(a, b, c);
+
+        [TestCase(123413, 943578, 123413, 943578, 943578, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(12, 21, 91, 17, 0, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(-10, 35, 90, 55, -105, ExpectedResult = 5)]
+        [TestCase(1, 213124, -54654, -123124, 65765, 44444, -7, 1234567, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(0, 1, 0, 0, ExpectedResult = 1)]
+        [TestCase(18, 3, 9, 6, ExpectedResult = 3)]
+        [TestCase(0, 0, 1, 0, ExpectedResult = 1)]
+        [Timeout(3000)]
+        public int GcdBySteinTests_ManyNumbers(params int[] numbers) => GetGcdByEuclidean(numbers);
+
+        [TestCase(123413, 943578, 123413, 943578, 943578, int.MaxValue)]
+        [TestCase(12, 21, 91, 17, 0, int.MaxValue)]
+        [TestCase(-10, 35, 90, 55, -105)]
+        [TestCase(1, 213124, -54654, -123124, 65765, 44444, -7, 1234567, int.MaxValue)]
+        [TestCase(0, 1, 0, 0)]
+        [TestCase(18, 3, 9, 6)]
+        [TestCase(0, 0, 1, 0)]
+        [Timeout(3000)]
+        public void GcdBySteinTests_WithTime(params int[] numbers)
+        {
+            _ = GetGcdByEuclidean(out long time, numbers);
+            Assert.That(time > 1);
+        }
+
+        #endregion
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -8,7 +8,7 @@ namespace Task2
     public static class IntegerExtensions
     {
         /// <summary>
-        ///   <para>Gets the GCD of two integer numbers.</para>
+        ///   <para>Gets the GCD of two integer numbers using Euclidian algorithm.</para>
         ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
         /// </summary>
         /// <param name="a">First number.</param>
@@ -34,7 +34,7 @@ namespace Task2
         }
 
         /// <summary>
-        ///   <para>Gets the GCD of three integer numbers.</para>
+        ///   <para>Gets the GCD of three integer numbers using Euclidian algorithm.</para>
         ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
         /// </summary>
         /// <param name="a">First number.</param>
@@ -64,7 +64,7 @@ namespace Task2
         }
 
         /// <summary>
-        ///   <para>Gets the GCD of several integer numbers.</para>
+        ///   <para>Gets the GCD of several integer numbers using Euclidian algorithm.</para>
         ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
         /// </summary>
         /// <param name="numbers">Given numbers.</param>
@@ -119,7 +119,7 @@ namespace Task2
         }
 
         /// <summary>
-        ///   <para>Gets the GCD of two integer numbers.</para>
+        ///   <para>Gets the GCD of two integer numbers using Euclidian algorithm.</para>
         ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
         /// </summary>
         /// <param name="a">First number.</param>
@@ -139,7 +139,7 @@ namespace Task2
         }
 
         /// <summary>
-        ///   <para>Gets the GCD of three integer numbers.</para>
+        ///   <para>Gets the GCD of three integer numbers using Euclidian algorithm.</para>
         ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
         /// </summary>
         /// <param name="a">First number.</param>
@@ -160,7 +160,7 @@ namespace Task2
         }
 
         /// <summary>
-        ///   <para>Gets the GCD of several integer numbers.</para>
+        ///   <para>Gets the GCD of several integer numbers using Euclidian algorithm.</para>
         ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
         /// </summary>
         /// <param name="time">Method's execution time in ticks.</param>
@@ -173,6 +173,176 @@ namespace Task2
             var timer = new Stopwatch();
             timer.Start();
             int result = GetGcdByEuclidean(numbers);
+            timer.Stop();
+            time = timer.ElapsedTicks;
+            return result;
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of two integer numbers using Stein's algorithm.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second number.</param>
+        /// <returns>Returns GCD of two numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when two numbers are zero.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByStein(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a == int.MinValue || b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException($"{int.MinValue} is out of range.");
+            }
+
+            int absA = Math.Abs(a);
+            int absB = Math.Abs(b);
+            return SteinsAlgorithm(absA, absB);
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of three integer numbers using Stein's algorithm.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second number.</param>
+        /// <param name="c">Third number.</param>
+        /// <returns>Returns GCD of three numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when three numbers are zero.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByStein(int a, int b, int c)
+        {
+            if (a == 0 && b == 0 && c == 0)
+            {
+                throw new ArgumentException("Three numbers cannot be 0 at the same time.");
+            }
+
+            if (a == int.MinValue || b == int.MinValue || c == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException($"{int.MinValue} is out of range.");
+            }
+
+            int absA = Math.Abs(a);
+            int absB = Math.Abs(b);
+            int absC = Math.Abs(c);
+
+            int temporaryResult = SteinsAlgorithm(absA, absB);
+            return SteinsAlgorithm(temporaryResult, absC);
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of several integer numbers using Stein's algorithm.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="numbers">Given numbers.</param>
+        /// <returns>Returns GCD of several numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are zero or less than two numbers was given.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByStein(params int[] numbers)
+        {
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException("There are should be at least two parameters.");
+            }
+
+            var gcdsArray = new List<int>(numbers);
+            bool hasAllZeroes = true;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == int.MinValue)
+                {
+                    throw new ArgumentOutOfRangeException($"{int.MinValue} is out of range.");
+                }
+
+                gcdsArray[i] = Math.Abs(numbers[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    hasAllZeroes = false;
+                    break;
+                }
+            }
+
+            if (hasAllZeroes)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.");
+            }
+
+            for (int i = 0; i < gcdsArray.Count - 1; i++)
+            {
+                if (gcdsArray[i] == 0)
+                {
+                    continue;
+                }
+
+                gcdsArray[i + 1] = EuclideanAlgorithm(gcdsArray[i + 1], gcdsArray[i]);
+            }
+
+            return gcdsArray[gcdsArray.Count - 1];
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of two integer numbers using Stein's algorithm.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second number.</param>
+        /// <param name="time">Method's execution time in ticks.</param>
+        /// <returns>Returns GCD of two numbers and method's execution time.</returns>
+        /// <exception cref="ArgumentException">Thrown when two numbers are zero.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByStein(int a, int b, out long time)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            int result = GetGcdByStein(a, b);
+            timer.Stop();
+            time = timer.ElapsedTicks;
+            return result;
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of three integer numbers using Stein's algorithm.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second number.</param>
+        /// <param name="c">Third number.</param>
+        /// <param name="time">Method's execution time in ticks.</param>
+        /// <returns>Returns GCD of three numbers and method's execution time.</returns>
+        /// <exception cref="ArgumentException">Thrown when three numbers are zero.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByStein(int a, int b, int c, out long time)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            int result = GetGcdByStein(a, b, c);
+            timer.Stop();
+            time = timer.ElapsedTicks;
+            return result;
+        }
+
+        /// <summary>
+        ///   <para>Gets the GCD of several integer numbers using Stein's algorithm.</para>
+        ///   <para>Valid interval (<em>int.MinValue, int.MaxValue</em>].</para>
+        /// </summary>
+        /// <param name="time">Method's execution time in ticks.</param>
+        /// <param name="numbers">Given numbers.</param>
+        /// <returns>Returns GCD of several numbers and method's execution time.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are zero or less than two numbers was given.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one argument is equals to <em>int.Minvalue</em>.</exception>
+        public static int GetGcdByStein(out long time, params int[] numbers)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            int result = GetGcdByStein(numbers);
             timer.Stop();
             time = timer.ElapsedTicks;
             return result;
@@ -200,6 +370,50 @@ namespace Task2
             }
 
             return absA;
+        }
+
+        private static int SteinsAlgorithm(int absA, int absB)
+        {
+            if (absA == 0)
+            {
+                return absB;
+            }
+
+            if (absB == 0)
+            {
+                return absA;
+            }
+
+            int shift;
+            for (shift = 0; ((absA | absB) & 1) == 0; ++shift)
+            {
+                absA >>= 1;
+                absB >>= 1;
+            }
+
+            while ((absA & 1) == 0)
+            {
+                absA >>= 1;
+            }
+
+            do
+            {
+                while ((absB & 1) == 0)
+                {
+                    absB >>= 1;
+                }
+
+                if (absA > absB)
+                {
+                    int t = absB;
+                    absB = absA;
+                    absA = t;
+                }
+
+                absB = absB - absA;
+            }
+            while (absB != 0);
+            return absA << shift;
         }
     }
 }
